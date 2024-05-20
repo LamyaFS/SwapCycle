364db8353d1050f224599652a8953cd0d97a2935
Lamya Faizal Salma <130156095+LamyaFS@users.noreply.github.com>
2024-05-20 12:17:53 +0400

    L updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,17 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { initializeApp as initializeApp_alias, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth as getAuth_alias, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import {AngularFireModule} from '@angular/fire/compat';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,AngularFireModule,AngularFireModule.initializeApp(environment.firebase)],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideFirebaseApp(() => initializeApp({"projectId":"swapcycle-f5400","appId":"1:615718133563:web:5044123db87784ec8d211e","databaseURL":"https://swapcycle-f5400-default-rtdb.firebaseio.com","storageBucket":"swapcycle-f5400.appspot.com","apiKey":"AIzaSyCv8oOMIP6OR6T-twIKqyAanT3qoTPRC3w","authDomain":"swapcycle-f5400.firebaseapp.com","messagingSenderId":"615718133563"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   
