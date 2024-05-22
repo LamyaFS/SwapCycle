9a089a8d058cf78414d51d6dfa529c8c5e1801ad
Lamya Faizal Salma <130156095+LamyaFS@users.noreply.github.com>
2024-05-22 21:57:31 +0400

    LAuthClear

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { Router } from '@angular/router';
 import { initializeApp } from "firebase/app";
-import {environment} from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-login',
@@ -11,42 +10,23 @@ import {environment} from 'src/environments/environment';
 })
 export class LoginPage implements OnInit {
 
-  oApp = initializeApp(environment.firebase);
-  oAuth = getAuth(this.oApp);
   
-  gmail=""
-  password=""
 
   constructor(private router: Router) { }
 
   login() {
-    console.log('Logging in with:', this.gmail, this.password);
+    
     this.router.navigate(['/home']);
   }
 
   forgotPassword() {
-    console.log('Forgot Password clicked');
     this.router.navigate(['/forgot-password']);
   }
 
-  signup() {    
-    console.log('Register clicked');   
+  signup() {   
     this.router.navigate(['/signup']);
   }
   tomain() {
-    const auth = getAuth();
-    signInWithEmailAndPassword(this.oAuth, this.gmail, this.password)
-      .then((userCredential) => {
-        const user = userCredential.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
-
       this.router.navigate(['/main']);
   }
 
