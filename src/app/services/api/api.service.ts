f44af816a521b6ca9e7c3b35e1eee2e44b7df5c0
tahm1na <tahminanasrin04.com>
2024-06-02 00:54:17 +0400

    noprice

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -27,36 +27,36 @@ export class ApiService {
     {
       id: '3',
       name: 'Wooden desk',
-      price: 5000,
+      
       status: true,
-      rating: 4.9,
+      
       cover: 'assets/Products/woodenDesk.jpeg',
       description: 'Dark stained wooden desk with drawer'
     },
     {
       id: '4',
       name: 'Lenovo Laptop',
-      price: 5000,
+      
       status: true,
-      rating: 4.9,
+      
       cover: 'assets/Products/laptop.jpeg',
       description: 'Lenovo ThinkPad L450-20DT 15.6" Intel Core i5-4300U'
     },
     {
     id: '5',
       name: 'Sofa set',
-      price: 500,
+      
       status: true,
-      rating: 5.0,
+      
       cover: 'assets/Products/sofa set.jpg',
       description: 'Sofa'
     },
     {
       id: '6',
         name: 'Washing machine',
-        price: 500,
+        
         status: true,
-        rating: 5.0,
+        
         cover: 'assets/Products/WM.webp',
         description: 'White automatic washing machine'
       }
