68dd8c7bb81365ab8507495815056b5e4596b411
Lamya Faizal Salma <130156095+LamyaFS@users.noreply.github.com>
2024-06-06 12:17:03 +0400

    Geo TALA

--- src/app/product/product.page.ts
+++ src/app/product/product.page.ts
@@ -3,8 +3,10 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 import { HttpClient } from '@angular/common/http';
 import {Router} from '@angular/router';
+import { Geolocation } from '@capacitor/geolocation';
+import { getDatabase, ref, query, orderByChild, equalTo, DataSnapshot , onValue} from "firebase/database";
 
-
+declare var google: any;
 @Component({
   selector: 'app-product',
   templateUrl: './product.page.html',
@@ -16,10 +18,23 @@ export class ProductPage implements OnInit {
   Condition: string = '';
   TimeSlot: string = '';
   Day: string='';
+  
+  currentLat: number;
+  currentLon: number;
+  mapElementRef: any;
+  map: any;
+  service: any;
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {
 
-  ngOnInit() {}
+    this.currentLat=0;
+    this.currentLon=0;
+
+  }
+
+  ngOnInit() {
+    this.loadMap();
+  }
 
   async takePicture() {
     const image = await Camera.getPhoto({
@@ -32,7 +47,28 @@ export class ProductPage implements OnInit {
     this.productImage = `data:image/jpeg;base64,${image.base64String}`;
   }
 
+
+  async getPosition()
+  {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.currentLat = coordinates.coords.latitude;
+    this.currentLon = coordinates.coords.longitude;
+  }
+  async loadMap() {
+    await this.getPosition();
+    let mapOptions = {
+      center: { lat: this.currentLat, lng: this.currentLon },
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    this.mapElementRef = document.getElementById("map");
+    this.map = new google.maps.Map(this.mapElementRef, mapOptions);
+    this.service = new google.maps.places.PlacesService(this.map);
+    let currentLocation = { lat: this.currentLat, lng: this.currentLat };
+    
+  }
   uploadProduct() {
+    
     this.router.navigate(['tabs/main'])
   }
 }
